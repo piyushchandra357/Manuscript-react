a744ea19e2908b808febd0306b9614ada50b4e40
Piyush Chandra <piyush.chandra2013@gmail.com>
2021-07-05 21:08:44 +0530

    Update 2

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Manuscript</h1>
+        </nav>
+    )
+}
---
+++ src/components/icons/logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <path
+          d="M 34 49 L 34 66 L 29 66 L 29 29 L 42 29 C 51 29 51.53 36.238 51.549 38.999 C 51.426 42.427 51 49 42 49 Z M 34 44 L 36 44 C 43.507 43.969 46.046 43.031 46.098 38.928 C 45.994 33.973 43.463 34.012 36 34 L 34 34 M 34 44 Z Z"
+          fill="currentColor"
+        />
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default IconLogo;
